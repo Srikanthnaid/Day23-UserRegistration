cb9f08c03ed2f938465b3a30ceb6131201b9ae21
Srikanthnaid <ksru044gmail.com>
2022-12-08 10:31:14 +0530

    Added:SecondName

--- lib/src/main/java/Day23/UserRegistration/UserRegistration.java
+++ lib/src/main/java/Day23/UserRegistration/UserRegistration.java
@@ -1,20 +1,45 @@
 package Day23.UserRegistration;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		//call the interface methods;
-		Function function = (FirstName) -> {//lambda expression
-			if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", FirstName)) {
-				System.out.println("Valid");
-			} else {
-				System.out.println("! Not-Valid");
-			}
-		};
-		function.check("Srikanth");//Pass the argument firstname
+		UserRegistration userRegistration = new UserRegistration();
+		userRegistration.validFirstName();
+		userRegistration.validSecondName();
+	}
+
+	public void validFirstName() {
+		Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter user firstname");
+		String firstName = scanner.next();
+
+		Matcher matcher = pattern.matcher(firstName);
+		if (matcher.matches()) {
+			System.out.println("First name is valid");
+		} else {
+			System.out.println("First name is Invalid");
+		}
+	}
+
+	public void validSecondName() {
+		Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter user second name");
+		String secondName = scanner.next();
+
+		Matcher matcher = pattern.matcher(secondName);
+		if (matcher.matches()) {
+			System.out.println("Second name is valid");
+		} else {
+			System.out.println("Second name is Invalid");
+		}
 	}
 
 }
